b75d6e926eef037fb13df9f8b1b3c154a8b3a74a
bhp <bhp@easv.dk>
2019-02-22 11:27:58 +0100

    Add project files.

---
+++ Exercise 9/Program.cs
@@ -0,0 +1,90 @@
+﻿using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter search string: ");
+            string searchString = Console.ReadLine().Trim();
+
+            WebClient wc = new WebClient();
+            Queue<Uri> initialQueue = InitialSearch(searchString);
+            Console.WriteLine("Initial queue size = " + initialQueue.Count);
+
+            // Initiate the first crawler
+            List<WebCrawler> crawlers = new List<WebCrawler>();
+            crawlers.Add(new WebCrawler(searchString, initialQueue, 6));
+
+            // Initiate 9 more worker crawlers
+            for (int i = 1; i < 10; i++)
+            {
+                crawlers.Add(new WebCrawler());
+            }
+
+            Console.WriteLine("\nCrawling...");
+
+            bool done = false;
+            while (!done)
+            {
+
+                Console.WriteLine("\nPress any key to see status (ESC to stop)...");
+
+                // if ESC is pressed then stop
+                if (Console.ReadKey().Key == ConsoleKey.Escape) done = true;
+
+                // Prints out the size of the frontier queue
+                Console.WriteLine("\nFrontier size = " + crawlers[0].getFrontierSize());
+            }
+
+            // Stop all crawlers
+            for (int i = 0; i < 10; i++)
+            {
+                crawlers[i].Stop();
+            }
+
+            // Get all visited urls
+            Dictionary<Uri, bool> visitedUrls = crawlers[0].GetVisitedUrls();
+
+            PrintResults(crawlers[0].GetVisitedUrls(), crawlers[0].GetResultUrls());
+        }
+
+        private static void PrintResults(Dictionary<Uri, bool> visitedUrls, Queue<Uri> results)
+        {
+            // Print out the number of visited urls
+            Console.WriteLine("\nNumber of visited urls = " + visitedUrls.Count);
+
+            // Print out the result urls containing the search string found by the web crawlers 
+
+            foreach (Uri url in results)
+                Console.WriteLine(url);
+
+            // Print out the number of results found by the crawlers
+            Console.WriteLine("\nNumber of found results = " + results.Count);
+        }
+
+        private static Queue<Uri> InitialSearch(string searchString)
+        {
+            string apiKey = "AIzaSyDs1yfYiY1gVtAC-40B5rpNgFLQqmbuN04";
+            string cx = "015351139142946479508:3lihax8uevs";
+            string query = searchString;
+
+            var svc = new Google.Apis.Customsearch.v1.CustomsearchService(new BaseClientService.Initializer { ApiKey = apiKey });
+            var listRequest = svc.Cse.List(query);
+
+            listRequest.Cx = cx;
+            var search = listRequest.Execute();
+
+            Queue<Uri> links = new Queue<Uri>();
+            foreach (var result in search.Items)
+            {
+                links.Enqueue(new UriBuilder(result.Link).Uri);
+            }
+            return links;
+        }
+    }
+}
---
+++ Exercise 9/WebCrawler.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Exercise_9
+{
+    class WebCrawler
+    {
+        private static ConcurrentDictionary<Uri, bool> visitedUrls =
+            new ConcurrentDictionary<Uri, bool>();
+
+        private static BlockingCollection<Uri> resultUrls =
+            new BlockingCollection<Uri>(new ConcurrentQueue<Uri>());
+
+        private static BlockingCollection<KeyValuePair<Uri, int>> frontier =
+            new BlockingCollection<KeyValuePair<Uri, int>>(new ConcurrentQueue<KeyValuePair<Uri, int>>());
+
+        private static int maxLevel;
+        private static string searchTerm;
+
+        private Thread crawler;
+        private WebClient wc;
+        private bool done;
+
+        public WebCrawler(string aSearchTerm, Queue<Uri> urls, int maximumLevel)
+           : this()
+        {
+            searchTerm = aSearchTerm;
+            maxLevel = maximumLevel;
+            foreach (Uri url in urls)
+                frontier.Add(new KeyValuePair<Uri, int>(url, 0));
+        }
+
+        public WebCrawler()
+        {
+            wc = new WebClient();
+            wc.Headers.Add(HttpRequestHeader.UserAgent, "User-Agent");
+            //wc.Headers.Add(HttpRequestHeader.From, "bhp@easv.dk");
+            crawler = new Thread(new ThreadStart(crawl));
+            crawler.Start();
+        }
+
+        private void crawl()
+        {
+            done = false;
+            while (!done)
+            {
+                try
+                {
+                    KeyValuePair<Uri, int> keyValue = frontier.Take();
+                    Uri page = keyValue.Key;
+                    int level = keyValue.Value;
+                    resolvePage(page, level);
+                }
+                catch (ThreadInterruptedException)
+                {
+                    // nothing special to do, just wake up !
+                }
+            }
+
+        }
+
+        private void resolvePage(Uri page, int level)
+        {
+            if (visitedUrls.ContainsKey(page)) return;
+
+            visitedUrls[page] = true;
+
+            try
+            {
+                // try to download the webpage. Throws exception if the url is bad.
+                string webPage = wc.DownloadString(page.ToString());
+
+                // If the webpage does not contain the search term, just skip it.
+                if (!webPage.ToLower().Contains(searchTerm.ToLower())) return;
+
+                // Page contains search term, so add it to the results.
+                resultUrls.Add(page);
+
+                if (level < maxLevel)
+                {
+                    // look for links in the webpage
+                    var urlTagPattern = new Regex(@"<a.*?href\s*=\s*[""'](?<url>.*?)[""'].*?</a>", RegexOptions.IgnoreCase);
+
+                    var urls = urlTagPattern.Matches(webPage);
+
+                    Uri baseUrl = new UriBuilder(page.Host).Uri;
+
+                    foreach (Match url in urls)
+                    {
+                        try
+                        {
+                            string newUrl = url.Groups["url"].Value;
+                            Uri result;
+                            Uri.TryCreate(baseUrl, newUrl, out result);
+
+
+                            // if the url is for a webpage, add it to the frontier for crawling
+                            if (result.HostNameType == UriHostNameType.Dns && !visitedUrls.ContainsKey(result))
+                            {
+                                frontier.Add(new KeyValuePair<Uri, int>(result, level + 1));
+                            }
+                        }
+                        catch
+                        {
+                            //just continue with the next found link...
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Unable to load page
+                visitedUrls[page] = false;
+            }
+        }
+
+        public void Stop()
+        {
+            done = true;
+            if (crawler.ThreadState == ThreadState.WaitSleepJoin)
+                crawler.Interrupt();
+        }
+
+        public int getFrontierSize()
+        {
+            return frontier.Count;
+        }
+
+        public Dictionary<Uri, bool> GetVisitedUrls()
+        {
+            return new Dictionary<Uri, bool>(visitedUrls);
+        }
+
+        public Queue<Uri> GetResultUrls()
+        {
+            return new Queue<Uri>(resultUrls);
+        }
+    }
+}
